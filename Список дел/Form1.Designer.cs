b2bdc3b41106db01b2bb5288b1dcc68210ee0743
vsevo <vsevo@ENVI4>
2023-02-09 00:10:54 +0300

    Добавьте файлы проекта.

---
+++ Список дел/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿
+namespace Список_дел
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(8, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 290);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(291, 50);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(250, 290);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(597, 50);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(250, 290);
+            this.listBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cделать";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "В процессе";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(694, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Готово";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(624, 435);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 35);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить готовое";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(251, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 38);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "→";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button4.Location = new System.Drawing.Point(554, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 36);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "↞";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button5.Location = new System.Drawing.Point(251, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(37, 36);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "↠";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button6.Location = new System.Drawing.Point(552, 260);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 36);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "↠";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button7.Location = new System.Drawing.Point(552, 299);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(37, 38);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "→";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.button8.Location = new System.Drawing.Point(554, 52);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(35, 36);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "←";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Сегодня:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(75, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 19;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(251, 50);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(37, 38);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "←";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.button10.Location = new System.Drawing.Point(251, 94);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(37, 36);
+            this.button10.TabIndex = 21;
+            this.button10.Text = "↞";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(624, 394);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(199, 35);
+            this.button11.TabIndex = 22;
+            this.button11.Text = "Сохранить данные";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(624, 353);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(199, 35);
+            this.button12.TabIndex = 23;
+            this.button12.Text = "Загрузить  данные";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 353);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 435);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label9.Location = new System.Drawing.Point(16, 412);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 13);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Исполнитель";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 409);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.TabIndex = 24;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(133, 382);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(199, 20);
+            this.dateTimePicker2.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label7.Location = new System.Drawing.Point(16, 356);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Описание";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label8.Location = new System.Drawing.Point(16, 388);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Дата";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 480);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Список дел";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Список дел/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Список_дел
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label6.Text = DateTime.Now.ToString("yyyy.MM.dd"); // Текущая дата 
+        }
+
+
+
+        class Task
+        {
+            public string Description { get; set; }
+            public DateTime Date { get; set; }
+            public string Executor { get; set; }
+            
+
+            public Task(string description, DateTime date, string executor)
+            {
+                Description = description;
+                Date = date;
+                Executor = executor; 
+            }
+        }
+
+        private List<Task> tasks = new List<Task>();
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && dateTimePicker2.Value != null && textBox2.Text != "" )
+            {
+                Task newTask = new Task(textBox1.Text, dateTimePicker2.Value, textBox2.Text);
+                tasks.Add(newTask);
+                listBox1.Items.Add(newTask.Description + ", " + newTask.Date + ", " + newTask.Executor + ", ");
+                textBox1.Clear();
+                dateTimePicker2.Value = DateTime.Now;
+                textBox2.Clear();
+                
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) // Программирование кнопки на перемещение одного элемента
+        {
+
+            MoveSelectedItems(listBox1, listBox2);
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Программирование кнопки на очистку элементов
+        {
+            listBox3.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Программирование кнопки на перемещение одного элемента
+        {
+            MoveSelectedItems(listBox2, listBox3);
+        }
+
+        private void MoveSelectedItems(ListBox lstFrom, ListBox lstTo) // процедура перемещения выбранного элемента из одной табличной части в другую
+        {
+            while (lstFrom.SelectedItems.Count > 0)
+            {
+                string item = (string)lstFrom.SelectedItems[0];
+                lstTo.Items.Add(item);
+                lstFrom.Items.Remove(item);
+            }
+        }
+
+        private void MoveAllItems(ListBox lstFrom, ListBox lstTo) // процедура перемещения всех элементов из одной табличной части в другую
+        {
+            {
+                lstTo.Items.AddRange(lstFrom.Items);
+                lstFrom.Items.Clear();
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Программирование кнопки на перемещение всех элементов
+        {
+            MoveAllItems(listBox1, listBox2);
+        }
+
+        private void button6_Click(object sender, EventArgs e)// Программирование кнопки на перемещение всех элементов
+        {
+            MoveAllItems(listBox2, listBox3);
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Программирование кнопки перемещение одного элемента
+        {
+            MoveSelectedItems(listBox3, listBox2);
+        }
+
+        private void button8_Click(object sender, EventArgs e) // Программирование кнопки на перемещение всех элементов
+        {
+            MoveAllItems(listBox3, listBox2);
+        }
+
+        private void button9_Click(object sender, EventArgs e) // Программирование кнопки на перемещение одного элемента
+        {
+            MoveSelectedItems(listBox2, listBox1);
+        }
+
+        private void button10_Click(object sender, EventArgs e) // Программирование кнопки на перемещение всех элементов
+        {
+            MoveAllItems(listBox2, listBox1);
+        }
+
+        //Cохранение данных в файл
+        private void button11_Click(object sender, EventArgs e) // Процедура на выгрузку данных
+        {
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Text documents (.txt)|*.txt";
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter w = new StreamWriter(save.FileName);
+
+                foreach (var item in listBox1.Items)
+                    w.WriteLine(item.ToString());
+                foreach (var item in listBox2.Items)
+                    w.WriteLine(item.ToString());
+                foreach (var item in listBox3.Items)
+                    w.WriteLine(item.ToString());
+
+                w.Close();
+            }
+
+        }
+
+        private void button12_Click(object sender, EventArgs e) // Процедура на загруку данных
+        {
+            using (OpenFileDialog List = new OpenFileDialog())
+            {
+                List.Filter = "Текстовые файлы|*.txt";
+                if (List.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.AddRange(File.ReadAllLines(List.FileName));
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
